7d2396d8b3f0649d43634059e8f738e8ca75653a
RafaFigueiroa <rafael.00000847851@unicap.br>
2024-03-08 12:53:07 -0300

    tela de login

--- app/src/main/java/com/example/apppdm/MainActivity.java
+++ app/src/main/java/com/example/apppdm/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.apppdm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signin();
+        forgotPassword();
+        joinNow();
+    }
+
+    private void signin() {
+        TextView email = (TextView) findViewById(R.id.inputEmail);
+        TextView password = (TextView) findViewById(R.id.inputPassword);
+        Button signInButton = (Button) findViewById(R.id.signin);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "email: " + email.getText() + "\nsenha: " + password.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void forgotPassword() {
+        Button forgotPasswordButton = (Button) findViewById(R.id.forgotpassword);
+
+        forgotPasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "esqueci a senha", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void joinNow() {
+        Button joinNowButton = (Button) findViewById(R.id.joinnow);
+
+        joinNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "entrar agora", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
