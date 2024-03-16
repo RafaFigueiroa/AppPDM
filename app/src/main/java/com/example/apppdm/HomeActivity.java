f36bf0d3d53555c88f12ba5d1f3f13edafc92c29
RafaFigueiroa <rafael.00000847851@unicap.br>
2024-03-16 10:31:12 -0300

    add the 'dial a number' function

--- app/src/main/java/com/example/apppdm/HomeActivity.java
+++ app/src/main/java/com/example/apppdm/HomeActivity.java
@@ -2,10 +2,13 @@ package com.example.apppdm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -15,6 +18,7 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         changeNumber();
+        dialPhone();
     }
 
     private void changeNumber() {
@@ -39,6 +43,21 @@ public class HomeActivity extends AppCompatActivity {
                 result--;
 
                 number.setText(String.valueOf(result));
+            }
+        });
+    }
+
+    private void dialPhone() {
+        TextView phone = (TextView) findViewById(R.id.inputPhone);
+        Button dialPhoneButton = (Button) findViewById(R.id.dialPhoneButton);
+
+        Intent i = new Intent(Intent.ACTION_DIAL);
+
+        dialPhoneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i.setData(Uri.parse("tel:" + phone.getText().toString()));
+                startActivity(i);
             }
         });
     }
